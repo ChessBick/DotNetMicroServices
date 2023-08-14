9c5726711b6452b75091afb2d9e12916a08452f3
Henkie Sandile Mahlangu <Sandile.Mahlangu@ktc-za.com>
2023-08-14 09:33:09 +0200

    Api testing

---
+++ Main/API/StudentAPI/Settings/StudentDatabaseSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentAPI.Settings
+{
+    public class StudentDatabaseSettings: IStudentDatabaseSettings
+    {
+        public string StudentsCollectionName { get; set; }
+        public string CoursesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+     
+    public interface IStudentDatabaseSettings
+    {
+        string StudentsCollectionName { get; set; }
+        string CoursesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
